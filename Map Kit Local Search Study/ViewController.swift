61f6ab61e9cadf936e6cc9b96066d5e9ea027063
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-07 14:01:07 +0300

    Initial Commit

---
+++ Map Kit Local Search Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map Kit Local Search Study
+//
+//  Created by Ömer Faruk Kılıçaslan on 7.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
